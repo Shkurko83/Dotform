7dd9b240d69abc7b7b0b4658d53622013562aef7
Shkurko-KYu <shkurko83@gmail.com>
2026-07-06 13:48:04 +0300

    Initial Commit

---
+++ Dotform/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dotform
+//
+//  Created by Butt Simpson on 06.07.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dotform/DotformApp.swift
@@ -0,0 +1,17 @@
+//
+//  DotformApp.swift
+//  Dotform
+//
+//  Created by Butt Simpson on 06.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct DotformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
